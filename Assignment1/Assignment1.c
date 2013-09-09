0dd01b917a0efaa6810114dab525096140d0d2fb
Felttrip <nthompson55@gmail.com>
2013-09-09 09:29:43 -0500

    Made some minor changes to walk and cleaned up the documentation again

--- Assignment1/Assignment1.c
+++ Assignment1/Assignment1.c
@@ -12,16 +12,15 @@
  *                                                             !
  * IDEA                                                        !
  *   Create a adjacency list, if durring the creation of the   !
- *   list if the edge is already created the list won't        !
+ *   list a vertex already has two edges, the graph will not   !
  *   be a simple cycle and fail.  If the list is created       !
  *   succesfully, a walk will be preformed. Durring this walk  !
- *   we will check to see if there are multiple cycles         !
- *   in the adjacency list, if not this must be a simple       !
- *   cycle. This should preform in linear time because durring !
- *   reading stage and graph that isn't a set of simple cycles !
- *   will be rejected.  This allows us to only have one pass   !
- *   on the created adjacency list to check that a walk can be !
- *   preformed.                                                !
+ *   we will check to see if we are able to reach all of the   !
+ *   verticies  This should preform in linear time because     !
+ *   durring reading stage and graph that isn't a set of simple!
+ *   cycles will be rejected.  This allows us to only have one !
+ *   pass on the created adjacency list to check that a walk   !
+ *   can be preformed on all verticies                         !
  *-------------------------------------------------------------*/
 
 #include <stdlib.h>
@@ -31,12 +30,13 @@
 //structure for adjacency node, each vertex will have its own set of nodes
 typedef struct _aNode{
     int index;  //index of vertex
-    struct _aNode *nextPtr; //pointer for next vertex
+    struct _aNode *nextPtr; //pointer for next vertex (not used for this assignment but my be used later)
 }aNode;                                                                   
 
 //function prototypes
 int readInput(char *filename);
 int buildList(int v1, int v2);
+int walk(void);
 
 //global array
 aNode *array[MAXV];
@@ -57,12 +57,12 @@ int main(int argc,char **argv)
   int eCheck=readInput(argv[1]);
   if(eCheck==1) //if file fails to read exit program
     return 1;
-  else if (eCheck==2)
+  else if (eCheck==2)//file fails the edge check done at reading
   {
     printf("NO");
     return 1;
   }
-  else
+  else  // file is able to be read all te way, do a walk.
   {
     walk();
   }
@@ -124,15 +124,15 @@ int buildList(int v1, int v2)
   {
     //comenting out this portion of code, may be usefull for future assignments    
 
-    /*aNode *tempVert = malloc(sizeof(aNode)); //create vertex
-    tempVert->index = v2;                    
-    tempVert->nextPtr = NULL;                 
-    aNode *curr=array[v1];
-    while(curr->nextPtr!=NULL) // step through adjacency list untill the end is reached
-    {
-      curr=curr->nextPtr;
-    }
-    curr->nextPtr=tempVert;*/
+    	/*aNode *tempVert = malloc(sizeof(aNode)); //create vertex
+    	tempVert->index = v2;                    
+    	tempVert->nextPtr = NULL;                 
+    	aNode *curr=array[v1];
+    	while(curr->nextPtr!=NULL) // step through adjacency list untill the end is reached
+    	{
+      	curr=curr->nextPtr;
+    	}
+    	curr->nextPtr=tempVert;*/
 
     //each edge is listed once so if a vertex has a second listing then it will fail
     return 1;
@@ -143,14 +143,14 @@ int buildList(int v1, int v2)
 //walk
 int walk(void)
 { 
-  int next = 1;
+  int next = max;  //set the starting vertex to max(a known vertex)
   int count = 0;
   do
   {
     next=array[next]->index;
     count++;
   }
-  while(next!=1);
+  while(next!=max);
   
   if(count==max)
     printf("YES");
